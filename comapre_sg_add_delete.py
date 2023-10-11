79cbee62653cf32c84483f04d4aee313492209c5
yousra shleibik <Yousra.Shleibik@du.edu>
2023-10-11 11:29:11 -0600

    Replaced the color of added and deleted nodes and edges
    Added--> Grey
    Deleted--> Yellow

--- comapre_sg_add_delete.py
+++ comapre_sg_add_delete.py
@@ -26,9 +26,9 @@ def detect_changes(graph1, graph2):
     deleted_nodes = nodes1_set - nodes2_set
 
     if added_nodes:
-        changes['nodes'] = [dict(json.loads(node_str), color=[255, 255, 0]) for node_str in added_nodes]  # Yellow
+        changes['nodes'] = [dict(json.loads(node_str), color=[128, 128, 128]) for node_str in added_nodes]  # Grey
     if deleted_nodes:
-        changes['nodes'] = [dict(json.loads(node_str), color=[128, 128, 128]) for node_str in deleted_nodes]  # Grey
+        changes['nodes'] = [dict(json.loads(node_str), color=[255, 255, 0]) for node_str in deleted_nodes]  # Yellow
 
     edges1_set = set(json.dumps(edge, sort_keys=True) for edge in graph1['edges'])
     edges2_set = set(json.dumps(edge, sort_keys=True) for edge in graph2['edges'])
@@ -37,9 +37,9 @@ def detect_changes(graph1, graph2):
     deleted_edges = edges1_set - edges2_set
 
     if added_edges:
-        changes['edges'] = [dict(json.loads(edge_str), color=[255, 255, 0]) for edge_str in added_edges]  # Yellow
+        changes['edges'] = [dict(json.loads(edge_str), color=[128, 128, 128]) for edge_str in added_edges]  # Grey
     if deleted_edges:
-        changes['edges'] = [dict(json.loads(edge_str), color=[128, 128, 128]) for edge_str in deleted_edges]  # Grey
+        changes['edges'] = [dict(json.loads(edge_str), color=[255, 255, 0]) for edge_str in deleted_edges]  # Yellow
 
     return changes
 
