b142c2991d5207ae938126dbb4e315408eca8156
yousra shleibik <Yousra.Shleibik@du.edu>
2023-10-11 10:51:25 -0600

    Adding files

---
+++ comapre_sg_add_delete.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 11 10:44:21 2023
+
+@author: yasoo
+"""
+
+import json
+
+def read_json(file_path):
+    with open(file_path, 'r') as f:
+        return json.load(f)
+
+def save_json(data, file_path):
+    with open(file_path, 'w') as f:
+        json.dump(data, f)
+
+def detect_changes(graph1, graph2):
+    changes = {}
+
+    # Serialize each node and edge to a string to make them hashable
+    nodes1_set = set(json.dumps(node, sort_keys=True) for node in graph1['nodes'])
+    nodes2_set = set(json.dumps(node, sort_keys=True) for node in graph2['nodes'])
+
+    added_nodes = nodes2_set - nodes1_set
+    deleted_nodes = nodes1_set - nodes2_set
+
+    if added_nodes:
+        changes['added_nodes'] = [dict(json.loads(node_str), color=[255, 255, 0]) for node_str in added_nodes]  # Yellow
+    if deleted_nodes:
+        changes['deleted_nodes'] = [dict(json.loads(node_str), color=[128, 128, 128]) for node_str in deleted_nodes]  # Grey
+
+    edges1_set = set(json.dumps(edge, sort_keys=True) for edge in graph1['edges'])
+    edges2_set = set(json.dumps(edge, sort_keys=True) for edge in graph2['edges'])
+
+    added_edges = edges2_set - edges1_set
+    deleted_edges = edges1_set - edges2_set
+
+    if added_edges:
+        changes['added_edges'] = [dict(json.loads(edge_str), color=[255, 255, 0]) for edge_str in added_edges]  # Yellow
+    if deleted_edges:
+        changes['deleted_edges'] = [dict(json.loads(edge_str), color=[128, 128, 128]) for edge_str in deleted_edges]  # Grey
+
+    return changes
+
+def main():
+    graph1 = read_json('old-dsg.json')
+    graph2 = read_json('new-dsg.json')
+    
+    changes = detect_changes(graph1, graph2)
+    
+    save_json(changes, 'changes_add_del.json')
+
+if __name__ == '__main__':
+    main()
